e0928860a52cc8d3d3078b0877f6c434c2a787af
ChukwudiNebo <chukwudineboch@gmail.com>
2024-01-03 01:00:37 -0800

    worked on the not found page

--- src/ui/pages/noFound/NotFound.tsx
+++ src/ui/pages/noFound/NotFound.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import notFoundImg from '../../../assets/notFound.png'
+import React from 'react';
+import notFoundImg from '../../../assets/noFound.png';
 import Button from '../../atoms/button/Button';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
-    <div>
-      <img src={notFoundImg} alt='' />
-      <p>
-        Oops! the page you're looking for doesn't exist. Double-check the link
-        or head back to the homepage.
-      </p>
-      <Button buttonText={'Go to Hompage'} />
+    <div className='notFound'>
+      <img src={notFoundImg} alt='' className='nofoundImg' />
+      <div className='notFound__paragraph'>
+        <p className='notFound__text'>
+          Oops! the page you're looking for doesn't exist. Double-check the link
+          or head back to the homepage.
+        </p>
+        <Link to='/' className='notFound__button'>
+          <Button buttonText={'Go to Hompage'} />
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
-export default NotFound
+export default NotFound;
